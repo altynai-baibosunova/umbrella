0af7c278ea2de6c87c5799f7ccdf7327ec0766f4
Altynai <harmoni5413@gmail.com>
2025-11-19 13:31:05 -0500

    User class

---
+++ src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    public String name;
+
+    public User(String name){
+        this.name=name;
+    }
+    public void sayHello(){
+        System.out.println("Hi, my name "+name);
+    }
+}
